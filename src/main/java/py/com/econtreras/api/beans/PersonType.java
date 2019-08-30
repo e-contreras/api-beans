bb8cbb28686e8f0ec41886b61f1760dbc44cd75c
Elvio Contreras <elviocontreras02@gmail.com>
2019-08-29 23:38:43 -0400

    Se agrega clases request para cVehicle y PersonalType

---
+++ src/main/java/py/com/econtreras/api/beans/PersonType.java
@@ -0,0 +1,21 @@
+package py.com.econtreras.api.beans;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class PersonType implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("id")
+    private Integer id;
+    @NotEmpty(message = "the field descrption can't be empty")
+    @NotNull(message = "the field descrption can't be null")
+    @JsonProperty("description")
+    private String description;
+    
+}
---
+++ src/main/java/py/com/econtreras/api/beans/Vehicle.java
@@ -0,0 +1,33 @@
+package py.com.econtreras.api.beans;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class Vehicle implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("id")
+    private Integer id;
+    @NotEmpty(message = "the field brand can't be empty")
+    @NotNull(message = "the field brand can't be null")
+    @JsonProperty("brand")
+    private String brand;
+    @JsonProperty("model")
+    private String model;
+    @JsonProperty("year")
+    private Integer year;
+    @JsonProperty("capacity")
+    private String capacity;
+    @JsonProperty("documentNumber")
+    private String documentNumber;
+    @JsonProperty("plate")
+    private String plate;
+    @JsonProperty("chassis")
+    private String chassis;
+
+}
